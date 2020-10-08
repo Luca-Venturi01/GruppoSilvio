3fd420e79d2298faea93ed442b63945a912bbab5
Lorenzo Gessi <lorenzo.gessi2@gmail.com>
2020-10-08 13:14:37 +0200

    Proseguo Compito2 da solo - Lorenzo

--- Compito2/esercizio2.c
+++ Compito2/esercizio2.c
@@ -17,10 +17,28 @@
 #include <stdlib.h>
 
 int main() {
+
+    /* ToDo */
+    // 1 - Sarebbe da controllare che il numero il numero max sia > di min
+    //     ma senza il while è un po' difficile, potrei fare uno scambio da codice se min > max magari
+    //     notificarlo poi all'utente.
+    // 2 - Fixare problema con getchar() --> appena i numeri vengono inseriti, il prompt si chiude.
+    //     La soluzione temporanea è stata quella di inserire system("PAUSE");
+
+    /* Dichiarazione delle variabili */
+    unsigned int cont = 0; // Rappresenta il numero di tentativi fatti per indovinare il numero
+    unsigned int min, max; // Rappresentano l'intervallo di ricerca inserito in input dall'utente.
+
     printf("Esercizio 2: Pensa a un numero\n");
-    printf("------------------------------");
+    printf("------------------------------\n");
+    printf("Inserisci l\'intervallo di ricerca (min max): ");
+    scanf("%d %d", &min, &max);
+    printf("Bene! Ora pensa a un numero compreso tra %d e %d\n", min, max);
+    
+    /* Codice per Testing */
+    // printf("Min: %d, Max: %d\n", min, max);
 
-
-    printf("\nPremi invio per chiudere il terminale.");
-    getchar();
+    // printf("\nPremi invio per chiudere il terminale.");
+    // getchar();
+    system("PAUSE");
 }

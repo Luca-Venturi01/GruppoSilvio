6192ff6bf4ef41962e2ed9b853a633d58411e70e
Lorenzo Gessi <lorenzo.gessi2@gmail.com>
2020-10-15 20:57:43 +0200

    Aggiunto Compito3 - LG

---
+++ Compito3/esercizio2.c
@@ -0,0 +1,23 @@
+/**HEADER********************************************************************
+ *  AUTORI: Lorenzo Gessi, Andrea Bedei, Fabio Notaro e Luca Venturi.
+ *  EMAIL:  lorenzo.gessi@studio.unibo.it, andrea.bedei2@studio.unibo.it, fabio.notaro2@studio.unibo.it, luca.venturi29@studio.unibo.it
+ *  DATA:   20201009
+ *  ES n.2: 
+***END************************************************************************/
+
+/* Inclusione delle librerie */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+int main() {
+
+    /* Dichiarazione delle variabili */
+
+    
+    printf("\n\n\nPremi invio per chiudere il terminale...");
+    getchar();
+    return 0;
+
+}

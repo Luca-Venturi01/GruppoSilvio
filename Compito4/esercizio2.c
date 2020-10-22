96e1f739d0cb2e8f2efcb4f875ebd36b866198b7
Lorenzo Gessi <lorenzo.gessi2@gmail.com>
2020-10-23 00:39:58 +0200

    Compattazione procede - LG

--- Compito4/esercizio2.c
+++ Compito4/esercizio2.c
@@ -2,8 +2,8 @@
  *  AUTORI: Lorenzo Gessi, Andrea Bedei, Fabio Notaro e Luca Venturi.
  *  EMAIL:  lorenzo.gessi@studio.unibo.it, andrea.bedei2@studio.unibo.it, fabio.notaro2@studio.unibo.it, luca.venturi29@studio.unibo.it
  *  DATA:   20201023
- *  ES n.2: Scrivere un programma C che legga N numeri interi da tastiera e li memorizzi in un vettore. 
-            Il programma deve generare un secondo vettore compattando i numeri contenuti nel primo vettore. 
+ *  ES n.2: Scrivere un programma C che legga N numeri interi da tastiera e li memorizzi in un vettore.
+            Il programma deve generare un secondo vettore compattando i numeri contenuti nel primo vettore.
             In particolare:
                 - ogni numero che compare ripetuto nel primo vettore, deve comparire una sola volta nel secondo vettore
                 - ogni numero uguale a zero presente nel primo vettore non deve comparire nel secondo vettore
@@ -16,21 +16,21 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define N_VALS 10
+// #define N_VALS 10
 
 int main() {
 
     // TODO:
     // 1 - Controllare se la fase di input, esempio: valore > x ecc. ecc.
-    // Refactoring? Meno codice stesse funzioni?
+    // 2 - Cercare un modo per creare pseudo-dinamicamente i nuovi vettori con i duplicati.
+    // 3 - Refactoring? Meno codice stesse funzioni? Usare operatore ternario.
 
     /* Dichiarazione delle Variabili */
     short int scanfReturn = 0; // Variabile utilizzata per controllare se la scanf ha ricevuto quello che si aspettava.
     int nVals = 0; // Variabile che rappresenta il numero di elementi che l'utente vuole inserire nel vettore.
     int random; // Rappresenta volta per volta il numero random generato da inserire nel vettore.
     int min, max; // Rappresentano l'intervallo, inserito in input dall'utente, di generazione di numeri random.
-    int arrayPt1[N_VALS]; // E' il vettore utilizzato nella prima parte.
-    int arrayPt2[N_VALS]; // E' il vettore utilizzato nella seconda parte.
+    int contDuplicati; // Variabile utilizzata per contare il numero di duplicati.
     bool inputCheck = false; // Variabile utilizzata per verificare che l'input dell'utente sia corretto.
 
     /* Inizio del Programma */
@@ -39,7 +39,24 @@ int main() {
     printf("Esercizio 2: Parte 1\n");
 
     /* Input dei dati */
-    for (int i = 0; i < N_VALS; i++)
+
+    // Richiedo e Controllo il numero di elementi che l'utente vuole inserire nel vettore.
+    while(!inputCheck){
+
+        printf("Quanti elementi vuoi inserire all'interno del primo vettore? ");
+        scanfReturn = scanf("%d", &nVals);
+        fflush(stdin);
+
+        if (nVals > 1 && scanfReturn == 1)
+            inputCheck = true;
+        else
+            printf("Inserire un valore valido, per favore.\n");
+    }
+    int arrayPt1[nVals]; // E' il vettore utilizzato nella prima parte.
+
+
+    // Inserisco gli elementi nel vettore.
+    for (int i = 0; i < nVals; i++)
     {
         printf("\nInserisci valore n.%d: ", i+1);
         scanfReturn = scanf("%d", &arrayPt1[i]);
@@ -53,35 +70,58 @@ int main() {
         }
         fflush(stdin);
     }
-    
+
     /* Output valori vettore */
     printf("\nI valori contenuti nel vettore sono: \n");
     printf("\tValori: --> ");
-    for (int i = 0; i < N_VALS-1; i++)
+    for (int i = 0; i < nVals-1; i++)
         printf("%d, ", arrayPt1[i]);
-    printf("%d <--\n", arrayPt1[N_VALS - 1]);
+    printf("%d <--\n", arrayPt1[nVals - 1]);
 
-    /* Compattazione */
-    for (int i = 0; i < N_VALS; i++)
+    /* Cerco i valori duplicati */
+    for (int i = 0; i < nVals; i++)
     {
-        for (int j = i + 1; j < N_VALS; j++)
+        for (int j = i + 1; j < nVals; j++)
         {
             if (arrayPt1[i] == arrayPt1[j])
             {
-                
+                printf("Duplicato: %d", arrayPt1[j]);
+                contDuplicati++;
+                // PROBLEMA: Se l'utente inserisce 3 volte il 44, questo viene considerato duplicato due volte.
             }
-            
         }
-        
+
     }
-    
+
+    /* Compattazione: Creo il nuovo array */
+    // // Cosí trovo gli elementi del vettore = 0.
+    //     if(arrayPt1[i] == 0)
+    //         continue;
+
 
     /* Parte 2: Con Random, No input utente */
     printf("\nEsercizio 2: Parte 2\n");
 
     /* Input dei dati */
+
+    // Richiedo e Controllo il numero di elementi che l'utente vuole inserire nel vettore.
+    inputCheck = false;
+    while(!inputCheck){
+
+        printf("Quanti elementi vuoi inserire all'interno del secondo vettore? ");
+        scanfReturn = scanf("%d", &nVals);
+        fflush(stdin);
+
+        if (nVals > 1 && scanfReturn == 1)
+            inputCheck = true;
+        else
+            printf("Inserire un valore valido, per favore.\n");
+    }
+    int arrayPt2[nVals]; // E' il vettore utilizzato nella seconda parte.
+
     // Controllo Min e Max richiedendo l'inserimento dei valori finchè max non è maggiore di min.
     // Codice riusabile, WOW!
+    inputCheck = false;
     while (!inputCheck)
     {
         printf("Inserisci l\'intervallo di generazione (min max): ");
@@ -99,15 +139,36 @@ int main() {
     }
 
     /* Generazione numeri random e inserimento nel vettore */
-	for(int i = 0; i < N_VALS; i++)
+	for(int i = 0; i < nVals; i++)
         arrayPt2[i] = min + (rand()+time(NULL))%(max+1-min); // Genera numeri tra min e max.
 
     /* Output valori vettore */
     printf("\nI valori contenuti nel vettore sono: \n");
     printf("\tValori: --> ");
-    for (int i = 0; i < N_VALS-1; i++)
+    for (int i = 0; i < nVals-1; i++)
         printf("%d, ", arrayPt2[i]);
-    printf("%d <--\n", arrayPt2[N_VALS - 1]);
+    printf("%d <--\n", arrayPt2[nVals - 1]);
+
+    /* Compattazione */
+    for (int i = 0; i < nVals; i++)
+    {
+        // Cosí trovo gli elementi del vettore = 0.
+        if(arrayPt2[i] == 0)
+            continue;
+        
+        // Cosí trovo i duplicati.
+        for (int j = i + 1; j < nVals; j++)
+        {
+            if (arrayPt2[i] == arrayPt2[j])
+            {
+                printf("Duplicato: %d", arrayPt2[j]);
+            }
+        }
+    
+        // Qui devo inserire gli elementi del vettore che sono diversi da quelli duplicati.
+        // some code goes here...
+
+    }
 
     // Comando che invita l'utente a premere il tasto "invio" per chiudere il programma.
     printf("\n\n\nPremi invio per chiudere il terminale...");
